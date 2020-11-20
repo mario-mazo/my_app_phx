380f893e95b0cd542c5784a9ce576978a7532529
Mario Mazo <buster.mazo@gmail.com>
2020-11-20 22:42:59 +0100

    sample app resources

---
+++ lib/my_app/resources/user.ex
@@ -0,0 +1,26 @@
+# in lib/my_app/resources/user.ex
+defmodule MyApp.User do
+  use Ash.Resource, data_layer: Ash.DataLayer.Ets
+
+  attributes do
+    attribute :email, :string,
+      allow_nil?: false,
+      constraints: [
+        match: ~r/^[\w.!#$%&’*+\-\/=?\^`{|}~]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*$/i
+      ],
+      primary_key?: true
+
+    attribute :id, :uuid, default: &Ecto.UUID.generate/0
+  end
+
+  actions do
+    create :default
+    read :default
+    update :default
+    destroy :default
+  end
+
+  relationships do
+    has_many :tweets, MyApp.Tweet, destination_field: :user_id
+  end
+end
